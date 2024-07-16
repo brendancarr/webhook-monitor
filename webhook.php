e3f9e991f0bfdafec84b7d5c8424dbc7351b1e73
Brendan Carr <brendan@carr.ca>
2024-07-16 10:35:47 -0700

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -1,5 +1,15 @@
 <?php
-// webhook.php
+/* webhook.php
+
+For receiving data without the extension, add this to your .htaccess:
+
+RewriteEngine On
+
+# Handle /webhook
+RewriteRule ^webhook$ webhook.php [L]
+
+*/
+
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $data = file_get_contents('php://input');
